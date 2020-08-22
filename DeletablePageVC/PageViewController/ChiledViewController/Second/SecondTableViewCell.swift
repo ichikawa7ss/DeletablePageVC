475f7719510e18b1642f4cc7283d0b76dfb770b6
ichikawa7ss <i2s7.chacha@gmail.com>
2020-08-22 18:39:43 +0900

    Add tableViewCell

---
+++ DeletablePageVC/PageViewController/ChiledViewController/Second/SecondTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  SecondTableViewCell.swift
+//  DeletablePageVC
+//
+//  Created by ichikawa on 2020/08/22.
+//  Copyright © 2020 ichikawa. All rights reserved.
+//
+
+import UIKit
+
+class SecondTableViewCell: UITableViewCell {
+
+    @IBOutlet private weak var label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    func setData(_ text: String) {
+        self.label.text = text
+    }
+}
--- DeletablePageVC/PageViewController/ChiledViewController/Second/SecondViewController.swift
+++ DeletablePageVC/PageViewController/ChiledViewController/Second/SecondViewController.swift
@@ -10,7 +10,26 @@ import UIKit
 
 final class SecondViewController: UIViewController {
 
+    @IBOutlet private weak var tableView: UITableView!
+    
+//    private var data = Array(repeating: "test", count: 20)
+    private var data = [Int](1...20)
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+// MARK: - DataSource
+extension SecondViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! SecondTableViewCell
+        cell.setData("No. \(String(self.data[indexPath.row]))")
+        return cell
     }
 }
